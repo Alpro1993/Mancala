f4f9701465a89ff7545137e19b0229f29fb27588
Alpro1993 <adaopinho25@gmail.com>
2021-11-12 11:40:59 +0000

    added more (non-functional) settings and handled usage of deprecated event tag

--- app.js
+++ app.js
@@ -7,35 +7,36 @@ function initial_config() {
 function assign_event_listeners() {
 
     //Overlay toggles
-    document.getElementById("settingsOverlay").addEventListener("click", function() {toggle_overlay("settingsOverlay")});
-    document.getElementById("rulesOverlay").addEventListener("click", function() {toggle_overlay("rulesOverlay")});
-    document.getElementById("leaderboardOverlay").addEventListener("click", function() {toggle_overlay("leaderboardOverlay")});
+    document.getElementById("settingsOverlay").addEventListener("click", function(eventObj) {toggle_overlay(eventObj, "settingsOverlay")});
+    document.getElementById("rulesOverlay").addEventListener("click", function(eventObj) {toggle_overlay(eventObj, "rulesOverlay")});
+    document.getElementById("leaderboardOverlay").addEventListener("click", function(eventObj) {toggle_overlay(eventObj, "leaderboardOverlay")});
     
     //Buttons
-    document.getElementById("settingsButton").addEventListener("click", function() {event.stopPropagation(); toggle_overlay("settingsOverlay")});
-    document.getElementById("rulesButton").addEventListener("click", function() {toggle_overlay("rulesOverlay")});
-    document.getElementById("leaderboardButton").addEventListener("click", function() {toggle_overlay("leaderboardOverlay")});
+    document.getElementById("settingsButton").addEventListener("click", function(eventObj) { toggle_overlay(eventObj, "settingsOverlay")});
+    document.getElementById("rulesButton").addEventListener("click", function(eventObj) {toggle_overlay(eventObj, "rulesOverlay")});
+    document.getElementById("leaderboardButton").addEventListener("click", function(eventObj) {toggle_overlay(eventObj, "leaderboardOverlay")});
 
     //Set overlay to off if you click outside it
-    document.addEventListener("click", function() {hide_overlay()});
+    document.addEventListener("click", function(eventObj) {hide_overlay(eventObj)});
 }
 
-function hide_overlay() {
+function hide_overlay(eventObj) {
     var overlays = document.getElementsByClassName("overlay");
     
     for(let i=0; i < overlays.length; i++) {
-        if(overlays[i].style.display == "block") {
-            toggle_overlay(overlays[i].id);
+        if(overlays[i].style.display == "flex") {
+            toggle_overlay(eventObj, overlays[i].id);
             return;
         }
     }
 }
 
-function toggle_overlay(overlayName) {
+function toggle_overlay(eventObj, overlayName) {
+    eventObj.stopPropagation();
     const overlay = document.getElementById(overlayName).style;
     const content = document.getElementById("content").style;
 
-    overlay.display === "block" ? (overlay.display = "none", content.filter = "") : (overlay.display = "block", content.filter = "blur(15px)");
+    overlay.display === "flex" ? (overlay.display = "none", content.filter = "") : (overlay.display = "flex", content.filter = "blur(15px)");
 }
 
 function load_board(size) {
